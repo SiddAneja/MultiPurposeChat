3f8e9db39225cb7a68c4ded8e68fa7c6241c976a
Siddharth Aneja <siddharth2001aneja@gmail.com>
2019-07-06 20:32:10 +0530

    gitignore

--- appServer/appMain.java
+++ appServer/appMain.java
@@ -1,7 +1,18 @@
 package appServer;
 
-public class appMain {
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.HashSet;
 
+public class appMain {
+  private static final int PORT = 59001;
+  private static HashSet<String> names = new HashSet<String>();
+  private static HashMap<String, Socket> clients = new HashMap<String, Socket>();
+  private static HashMap<String, ObjectOutputStream> map = new HashMap<>();
+
+  
+  
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
